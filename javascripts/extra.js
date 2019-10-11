e071d5a11289449ddc2db321e29ee2c44a3b8f81
bielduiarte <gabrielduarte.jb@gmail.com>
2019-10-11 17:02:06 -0300

    Deployed dc14919 with MkDocs version: 1.0.4

---
+++ javascripts/extra.js
@@ -0,0 +1,8 @@
+$.ajax({
+    url: "https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.1/js/lightbox.min.js",
+    dataType: "script",
+    success:function(){
+        console.log("deu certo")
+    }
+  });
+  
