c81e4168c5f91f99cce14888f3075b9f43ab26c4
biel duarte <gabrielduartejb@gmail.com>
2019-11-01 17:03:54 -0300

    alterando estrutura documentação

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -1,8 +1,17 @@
+
+$.ajax({
+    url: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js",
+    dataType: "script",
+    success:function(){
+        console.log("deu certo")
+    }
+  });
+
 $.ajax({
     url: "https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.1/js/lightbox.min.js",
     dataType: "script",
     success:function(){
         console.log("deu certo")
     }
+    
   });
-  
